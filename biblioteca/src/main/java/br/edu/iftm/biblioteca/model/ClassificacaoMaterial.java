006b9bb944aa1b8e108e72da01dfc2f413cdafc3
Laila Leal <laila.leal.ptc@gmail.com>
2026-04-01 09:19:06 -0300

    Adicao da classe modelo de Produto

---
+++ biblioteca/src/main/java/br/edu/iftm/biblioteca/model/ClassificacaoMaterial.java
@@ -0,0 +1,69 @@
+package br.edu.iftm.biblioteca.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Livros")
+public class ClassificacaoMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @Column(name = "autor")
+    private String autor;
+
+    @Column(name = "ano")
+    private Integer ano;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
