85960b80bb4cf072e05f154a70280089b8d6f000
Laila Leal <laila.leal.ptc@gmail.com>
2026-04-01 19:28:36 -0300

    Adicao da interface de classe de servico e a implementacao da classe de servico de produto

--- biblioteca/src/main/java/br/edu/iftm/biblioteca/controller/ClassificacaoMaterialController.java
+++ biblioteca/src/main/java/br/edu/iftm/biblioteca/controller/ClassificacaoMaterialController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class ProductController {
+public class ClassificacaoMaterialController {
 
     @GetMapping("/product")
     public String getMethodName() {
