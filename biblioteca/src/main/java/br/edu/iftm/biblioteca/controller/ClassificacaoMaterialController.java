6dd8884c2e6397c85ab5c2578668fa64f318787c
Laila Leal <laila.leal.ptc@gmail.com>
2026-04-08 21:09:49 -0300

    Adicao da funcionalidade de cadastrar produto

--- biblioteca/src/main/java/br/edu/iftm/biblioteca/controller/ClassificacaoMaterialController.java
+++ biblioteca/src/main/java/br/edu/iftm/biblioteca/controller/ClassificacaoMaterialController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.edu.iftm.biblioteca.model.ClassificacaoMaterial;
 import br.edu.iftm.biblioteca.service.ClassificacaoMaterialService;
 
 @Controller
@@ -19,4 +22,15 @@ public class ClassificacaoMaterialController {
         return "classificacaoMaterial/index";
     }
 
+    @GetMapping("/classificacaoMaterial/create")
+    public String create(Model model) {
+        model.addAttribute("material", new ClassificacaoMaterial());
+        return "classificacaoMaterial/create";
+    }
+
+    @PostMapping("/classificacaoMaterial/save")
+    public String save(@ModelAttribute("material") ClassificacaoMaterial material) {
+        classificacaoMaterialService.saveClassificacaoMaterial(material);
+        return "redirect:/classificacaoMaterial";
+    }
 }
